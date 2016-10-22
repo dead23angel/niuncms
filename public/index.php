c832fc4a33ca0b1a02e3186e1bf77c4653dd432d
Иван Горохов <cwteam@bk.ru>
2016-10-22 09:07:03 +0700

    First commit branch 3.0

--- public/index.php
+++ public/index.php
@@ -1,21 +1,58 @@
 <?php
+
 /**
- * The main bootstrap file.
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
  */
 
-session_start();
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| our application. We just need to utilize it! We'll simply require it
+| into the script here so that we don't have to worry about manual
+| loading any of our classes later on. It feels nice to relax.
+|
+*/
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__.'/../bootstrap/autoload.php';
 
-use App\Core;
-use Dotenv\Dotenv;
+/*
+|--------------------------------------------------------------------------
+| Turn On The Lights
+|--------------------------------------------------------------------------
+|
+| We need to illuminate PHP development, so let us turn on the lights.
+| This bootstraps the framework and gets it ready for use, then it
+| will load up this application so that we can run it and send
+| the responses back to the browser and delight our users.
+|
+*/
 
-$env = new Dotenv(__DIR__ . '/../');
-$env->load();
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-// Load the helpers
-require_once __DIR__ . '/../app/helpers.php';
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request
+| through the kernel, and send the associated response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have prepared for them.
+|
+*/
 
-$core = new Core();
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-$core->run();
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
